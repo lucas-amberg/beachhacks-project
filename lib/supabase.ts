01c489000fc9b2b0d6e59328c793b8eedea4f8d1
lucas-amberg <102396588+lucas-amberg@users.noreply.github.com>
2025-03-22 11:51:07 -0700

    Test

---
+++ lib/supabase.ts
@@ -0,0 +1,19 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+
+export type StudySet = {
+  id: number;
+  created_at: string;
+}
+
+export type StudyMaterial = {
+  id: string;
+  created_at: string;
+  study_set: number;
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export default supabase; 
